c1d6a5eeef1806d8f87485ecee72bf6229d140ed
Artem Sergeichev <a.sergeichev03@mail.ru>
2022-10-14 16:33:13 +0300

    creating reg

--- appSqlWpfLogisticCompany/RegistrationPage.xaml.cs
+++ appSqlWpfLogisticCompany/RegistrationPage.xaml.cs
@@ -24,24 +24,18 @@ namespace appSqlWpfLogisticCompany
         public RegistrationPage()
         {
             InitializeComponent();
-            genderCB.ItemsSource = DataBaseConnection.LogisticCompanyDB.Users_Gender.ToList();
-            genderCB.SelectedValuePath = "Code_Gender";
-            genderCB.DisplayMemberPath = "Name_Gender";
 
             roleCB.ItemsSource = DataBaseConnection.LogisticCompanyDB.Users_Roles.ToList();
             roleCB.SelectedValuePath = "Code_Role";
             roleCB.DisplayMemberPath = "Name_Role";
+
+            roleCB.SelectedIndex = 1;
+            roleCB.IsEnabled = false;
         }
 
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
             forFrameClass.publicFrame.Navigate(new RegistrationAuthorizationPage());
-            
-
-
-            
-
-            
         }
 
         private void registrationButton_Click(object sender, RoutedEventArgs e)
@@ -49,14 +43,22 @@ namespace appSqlWpfLogisticCompany
             //(?=.*[0-9]{2,})(?=.*[!@#$%^&*]{1,})(?=.*[a-z]{3,})(?=.*[A-Z]{1,})[0-9a-zA-Z!@#$%^&*]{8,}
 
             // https://www.codeproject.com/Articles/14537/The-Art-Science-of-Storing-Passwords
-            int genderId = genderCB.SelectedIndex + 1;
             int roleId = roleCB.SelectedIndex + 1;
             Regex checkPassword = new Regex("(?=.*[0-9]{2,})(?=.*[!@#$%^&*]{1,})(?=.*[a-z]{3,})(?=.*[A-Z]{1,})[0-9a-zA-Z!@#$%^&*]{8,}");
             string password = passwordUserRegistrationTB.Text;
 
+            if (nameUserRegistrationTB.Text == "") MessageBox.Show("Поле \"Имя\" не может быть пустым!");
+            else if (surnameUserRegistrationTB.Text == "") MessageBox.Show("Поле \"Фамилия\" не может быть пустым!");
+            else if (midnameUserRegistrationTB.Text == "") MessageBox.Show("Поле \"Отчество\" не может быть пустым!");
+            else if (loginUserRegistrationTB.Text == "") MessageBox.Show("Поле \"Логин\" не может быть пустым!");
+            else if (genderRBFemale.IsChecked == false && genderRBMale.IsChecked == false) MessageBox.Show("Выберете пол!");
+            else if (userBirthdayRegistrationDP.ToString() == "") MessageBox.Show("Укажите дату рождения!");
+            
+            int genderId;
+            if (genderRBMale.IsChecked == false) genderId = 2;
+            else genderId = 1;
             if (checkPassword.IsMatch(password))
             {
-                int passwordForSql = Convert.ToInt32(passwordUserRegistrationTB.GetHashCode().ToString());
                 Users RegistrationUser = new Users()
                 {
 
@@ -66,12 +68,14 @@ namespace appSqlWpfLogisticCompany
                     Login_User = loginUserRegistrationTB.Text,
                     Password_User = Convert.ToInt32(passwordUserRegistrationTB.GetHashCode().ToString()),
                     Code_Gender = genderId,
-                    Date_Birthday_User = Convert.ToDateTime(userBirthdayRegistrationDP),
+                    Date_Birthday_User = userBirthdayRegistrationDP.DisplayDate,
                     Code_Role = roleId,
                 };
                 DataBaseConnection.LogisticCompanyDB.Users.Add(RegistrationUser);
                 DataBaseConnection.LogisticCompanyDB.SaveChanges();
                 MessageBox.Show("Пользователь добавлен");
+                forFrameClass.publicFrame.Navigate(new RegistrationAuthorizationPage());
+                
 
             }
             else
