c6446c060c30bb2c306f234d46ce08da4b030599
Artem Sergeichev <a.sergeichev03@mail.ru>
2022-10-14 14:57:25 +0300

    creating registration page logic

--- appSqlWpfLogisticCompany/RegistrationPage.xaml.cs
+++ appSqlWpfLogisticCompany/RegistrationPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,18 +36,47 @@ namespace appSqlWpfLogisticCompany
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
             forFrameClass.publicFrame.Navigate(new RegistrationAuthorizationPage());
+            
+
+
+            
+
+            
+        }
+
+        private void registrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            //(?=.*[0-9]{2,})(?=.*[!@#$%^&*]{1,})(?=.*[a-z]{3,})(?=.*[A-Z]{1,})[0-9a-zA-Z!@#$%^&*]{8,}
+
+            // https://www.codeproject.com/Articles/14537/The-Art-Science-of-Storing-Passwords
             int genderId = genderCB.SelectedIndex + 1;
             int roleId = roleCB.SelectedIndex + 1;
+            Regex checkPassword = new Regex("(?=.*[0-9]{2,})(?=.*[!@#$%^&*]{1,})(?=.*[a-z]{3,})(?=.*[A-Z]{1,})[0-9a-zA-Z!@#$%^&*]{8,}");
+            string password = passwordUserRegistrationTB.Text;
 
-
-            Users RegistrationUser = new Users()
+            if (checkPassword.IsMatch(password))
             {
-                Name_User = nameUserRegistrationTB.Text,
-                Surname_User = surnameUserRegistrationTB.Text,
-                Midname_User = midnameUserRegistrationTB.Text,
-                Login_User = loginUserRegistrationTB.Text,
-                Code_Gender = genderId,
+                int passwordForSql = Convert.ToInt32(passwordUserRegistrationTB.GetHashCode().ToString());
+                Users RegistrationUser = new Users()
+                {
 
+                    Name_User = nameUserRegistrationTB.Text,
+                    Surname_User = surnameUserRegistrationTB.Text,
+                    Midname_User = midnameUserRegistrationTB.Text,
+                    Login_User = loginUserRegistrationTB.Text,
+                    Password_User = Convert.ToInt32(passwordUserRegistrationTB.GetHashCode().ToString()),
+                    Code_Gender = genderId,
+                    Date_Birthday_User = Convert.ToDateTime(userBirthdayRegistrationDP),
+                    Code_Role = roleId,
+                };
+                DataBaseConnection.LogisticCompanyDB.Users.Add(RegistrationUser);
+                DataBaseConnection.LogisticCompanyDB.SaveChanges();
+                MessageBox.Show("Пользователь добавлен");
+
+            }
+            else
+            {
+                MessageBox.Show("Ахтунг");
             }
         }
     }
