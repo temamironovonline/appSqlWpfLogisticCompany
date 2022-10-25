1898783f53b71962bf53bbe2d17fa4870b450269
temamironovonline <a.sergeichev03@mail.ru>
2022-10-25 07:34:58 +0300

    commenting RegistrationAuthorization page .cs and .xaml files

--- appSqlWpfLogisticCompany/RegistrationAuthorizationPage.xaml.cs
+++ appSqlWpfLogisticCompany/RegistrationAuthorizationPage.xaml.cs
@@ -26,72 +26,117 @@ namespace appSqlWpfLogisticCompany
         }
 
       
-
-        private void registrationButton_Click(object sender, RoutedEventArgs e)
+        
+        private void registrationButton_Click(object sender, RoutedEventArgs e) //Переход на страницу регистрации при нажатии на кнопку "Регистрация"
         {
             forFrameClass.publicFrame.Navigate(new RegistrationPage());
         }
 
-        private void loginButton_Click(object sender, RoutedEventArgs e)
+        private void loginButton_Click(object sender, RoutedEventArgs e) //Проверка пользователя при нажатии на кнопку "Войти"
         {
+         
             int checkPassword = userPasswordPB.Password.GetHashCode();
-            Users registratedUser = DataBaseConnection.LogisticCompanyDB.Users.FirstOrDefault(z => z.Login_User == userLoginTB.Text && z.Password_User == checkPassword);
-            if (registratedUser == null)
+            if(userLoginTB.Text == "Логин" || userPasswordPB.Password.ToString() == "") //Проверка на пустые поля "Логин" и "Пароль"
             {
-                MessageBox.Show("Неверный логин или пароль");
-            }
-            else
-            {
-                if (registratedUser.Code_Role == 2)
+                if(userLoginTB.Text == "Логин") emptyLogin.Visibility = Visibility.Visible; //Показываем ошибку пустого логина
+                if (userPasswordPB.Password.ToString() == "")
                 {
-                    MessageBox.Show("Добро пожаловать!");
+                    emptyPassword.Visibility = Visibility.Visible; //Показываем ошибку пустого пароля
+                    loginButton.Margin = new Thickness(0, 35, 0, 0);
+                }
+                
+            }
+            else 
+            {
+                Users registratedUser = DataBaseConnection.LogisticCompanyDB.Users.FirstOrDefault(z => z.Login_User == userLoginTB.Text && z.Password_User == checkPassword);
+                if (registratedUser == null) //Если пользователя не существует (или неверные введенные данные)
+                {
+                    wrongLoginPassword.Visibility = Visibility.Visible; //Показываем ошибку неверных данных
+                    wrongLoginPassword.Margin = new Thickness(0, 20, 0, 0);
+                    loginButton.Margin = new Thickness(0, 20, 0, 0);
                 }
                 else
                 {
-                    MessageBox.Show("Добро пожаловать, Господин!");
-                }
+                    if (registratedUser.Code_Role == 2)
+                    {
+                        MessageBox.Show("Добро пожаловать!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Добро пожаловать, Господин!");
+                    }
 
+                }
             }
+            
         }
 
-        private void userLoginTB_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void userLoginTB_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) //Событие при фокусе на поле ввода логина
         {
+            if (wrongLoginPassword.Visibility == Visibility.Visible) //Если ошибка неверных данных включена, то отключаем
+            {
+                wrongLoginPassword.Visibility = Visibility.Collapsed;
+                loginButton.Margin = new Thickness(0, 40, 0, 0);
+            }
+            if (emptyLogin.Visibility == Visibility.Visible) emptyLogin.Visibility = Visibility.Collapsed; //Если ошибка пустого логина видна, то отключаем
             var brushConverter = new BrushConverter();
             if (userLoginTB.Text == "Логин") userLoginTB.Text = "";
             userLoginTB.Foreground = (Brush)brushConverter.ConvertFrom("#FF6F97EE");
             polylineLogin.Stroke = (Brush)brushConverter.ConvertFrom("#FF6F97EE");
+            userLoginIcon.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/userloginBlue.png"));
+
         }
 
-        private void userLoginTB_LostFocus(object sender, RoutedEventArgs e)
+        private void userLoginTB_LostFocus(object sender, RoutedEventArgs e) //Событие при потере фокуса с поля ввода логина
         {
             var brushConverter = new BrushConverter();
             if (userLoginTB.Text == "") userLoginTB.Text = "Логин";
             userLoginTB.Foreground = (Brush)brushConverter.ConvertFrom("#C7C9C7");
             polylineLogin.Stroke = (Brush)brushConverter.ConvertFrom("#C7C9C7");
+            userLoginIcon.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/userlogin.png"));
         }
 
-        private void userPasswordTB_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void userPasswordTB_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) //Событие при фокусе на поле ввода пароля (TextBox) (если в поле пароль ничего не введено)
         {
+            if (emptyPassword.Visibility == Visibility.Visible) //Если ошибка пустого пароля видна, то отключаем
+            {
+                emptyPassword.Visibility = Visibility.Collapsed;
+                loginButton.Margin = new Thickness(0, 40, 0, 0);
+            }
             var brushConverter = new BrushConverter();
-            if (userPasswordTB.Text == "Пароль") userPasswordTB.Text = "";
-            
+            userPasswordTB.Visibility = Visibility.Collapsed;
+            userPasswordPB.Visibility = Visibility.Visible;
+            userPasswordPB.Focus();
             userPasswordTB.Foreground = (Brush)brushConverter.ConvertFrom("#FF6F97EE");
             polylinePassword.Stroke = (Brush)brushConverter.ConvertFrom("#FF6F97EE");
+            userPasswordIcon.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/passwordloginBlue.png"));
         }
 
-        private void userPasswordTB_LostFocus(object sender, RoutedEventArgs e)
+        private void userPasswordPB_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) //Событие при фокусе на поле ввода пароля (PasswordBox) (если в поле пароль что-то введено)
+        {
+            if (wrongLoginPassword.Visibility == Visibility.Visible) //Если ошибка неверных данных включена, то отключаем
+            {
+                wrongLoginPassword.Visibility = Visibility.Collapsed;
+                loginButton.Margin = new Thickness(0, 40, 0, 0);
+            }
+            var brushConverter = new BrushConverter();
+            polylinePassword.Stroke = (Brush)brushConverter.ConvertFrom("#FF6F97EE");
+            userPasswordIcon.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/passwordloginBlue.png"));
+        }
+
+        private void userPasswordPB_LostFocus(object sender, RoutedEventArgs e) //Событие при потере фокуса с поля ввода логина
         {
             var brushConverter = new BrushConverter();
-            if (userPasswordTB.Text == "") userPasswordTB.Text = "Пароль";
-            
+            if (userPasswordPB.Password.ToString() == "")
+            {
+                userPasswordPB.Visibility = Visibility.Collapsed;
+                userPasswordTB.Visibility = Visibility.Visible;
+            }
             userPasswordTB.Foreground = (Brush)brushConverter.ConvertFrom("#C7C9C7");
             polylinePassword.Stroke = (Brush)brushConverter.ConvertFrom("#C7C9C7");
+            userPasswordIcon.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/passwordlogin.png"));
         }
 
-        private void userPasswordTB_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            string password = userPasswordTB.Text;
-           
-        }
+       
     }
 }
