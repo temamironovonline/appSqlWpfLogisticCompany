d0c910c36916ba4565263ec72b3ee2dbc1d4cca1
Artem Sergeichev <a.sergeichev03@mail.ru>
2022-11-15 16:31:44 +0300

    addRequestPage editing

--- appSqlWpfLogisticCompany/addRequestPage.xaml.cs
+++ appSqlWpfLogisticCompany/addRequestPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,14 +10,20 @@ namespace appSqlWpfLogisticCompany
     /// </summary>
     public partial class addRequestPage : Page
     {
+        
         public addRequestPage()
         {
             InitializeComponent();
-            executorsComboBox.ItemsSource = DataBaseConnection.LogisticCompanyDB.Executors.ToList();
-            executorsComboBox.SelectedValuePath = "Code_Executor";
-            executorsComboBox.DisplayMemberPath = "Surname_Executor";
+            executorsComboBox.Items.Add("Не выбрано");
+            List<Executors> executorsList = DataBaseConnection.LogisticCompanyDB.Executors.ToList();
+            for(int i = 0; i < executorsList.Count; i++)
+            {
+                string fullName = $"{executorsList[i].Surname_Executor} {executorsList[i].Name_Executor} {executorsList[i].Midname_Executor}";
+                executorsComboBox.Items.Add(fullName);
+            }
+            executorsComboBox.Items.Add("Добавить нового исполнителя");
         }
-
+        
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
             forFrameClass.publicFrame.Navigate(new RequestsPage());
